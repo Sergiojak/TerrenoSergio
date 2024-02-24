b9973a873e42295c06397a05856ad5e5538a0775
Sergiojak <sergiohg.jak@gmail.com>
2024-02-24 21:18:08 +0000

    EnemyPatrolFinal

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -1,23 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    public Transform waypoint1;
-    public Transform waypoint2;
-    float enemyMovementSpeed = 1f;
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+    public GameObject enemy;
 
-    Transform enemy;
+    [SerializeField]
+    float enemyMovementSpeed = 20.0f;
+
+    bool enemyIsAtWaypoint1 = false;
+    bool enemyIsAtWaypoint2 = true;
+
     void Start()
     {
-        enemy = GetComponent<Transform>();
-        transform.position = waypoint1.position;
+      
     }
 
     void Update()
     {
-        transform.position += waypoint2.position * Time.deltaTime * enemyMovementSpeed;
+        if (enemyIsAtWaypoint1 == false)
+        {
+            enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, waypoint1.transform.position, enemyMovementSpeed * Time.deltaTime);        
+            transform.LookAt(waypoint1.transform.position);
+
+            /*Quaternion rotationTarget = Quaternion.LookRotation(enemy.transform.position, waypoint1.transform.position);
+            enemy.transform.rotation = Quaternion.RotateTowards(enemy.transform.rotation, rotationTarget, rotationSpeed * Time.deltaTime);*/
+
+            if (enemy.transform.position == waypoint1.transform.position) 
+            { 
+                enemyIsAtWaypoint1 = true;
+                enemyIsAtWaypoint2 = false;
+            }
+        }
+        if (enemyIsAtWaypoint2 == false)
+        {
+            enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, waypoint2.transform.position, enemyMovementSpeed * Time.deltaTime);
+            transform.LookAt(waypoint2.transform.position);
+
+            /*Quaternion rotationTarget2 = Quaternion.LookRotation(enemy.transform.position, waypoint2.transform.position);
+            enemy.transform.rotation = Quaternion.RotateTowards(enemy.transform.rotation, rotationTarget2, rotationSpeed * Time.deltaTime);*/
+
+            if (enemy.transform.position == waypoint2.transform.position)
+            {
+                enemyIsAtWaypoint1 = false;
+                enemyIsAtWaypoint2 = true;
+            }
+
+        }
     }
 }
